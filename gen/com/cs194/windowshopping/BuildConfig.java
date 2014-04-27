df9e5c9c5749e32b478526a89234a127d601f2f2
Jordan Marcy <jordan.marcy@gmail.com>
2014-04-27 10:55:32 -0700

    Initial commit

---
+++ gen/com/cs194/windowshopping/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.cs194.windowshopping;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/cs194/windowshopping/MainActivity.java
@@ -0,0 +1,39 @@
+package com.cs194.windowshopping;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    /*Called when a user clicks the Take Photo button*/
+    public void gotoCamera(View view) {
+    	
+    }
+    
+    /*Called when a user clicks the View Wishlist button*/
+    public void gotoWishlist(View view) {
+    	
+    }
+    
+    /*Called when a user clicks the Scan Barcode button*/
+    public void gotoScanner(View view) {
+    	
+    }
+    
+}
