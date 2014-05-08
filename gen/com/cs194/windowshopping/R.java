be5f73a0cb9867373e6721da9ff07e4416cfd2e4
Jordan Marcy <jordan.marcy@gmail.com>
2014-05-08 11:52:08 -0700

    basic backbone of app

--- gen/com/cs194/windowshopping/R.java
+++ gen/com/cs194/windowshopping/R.java
@@ -24,13 +24,25 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080004;
+        public static final int photoView=0x7f080000;
+        public static final int results=0x7f080001;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_photo=0x7f030001;
+        public static final int activity_results=0x7f030002;
+        public static final int activity_wishlist=0x7f030003;
+        public static final int item_view=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int photo=0x7f070001;
+        public static final int results=0x7f070002;
+        public static final int website=0x7f070003;
+        public static final int wishlist=0x7f070004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -39,6 +51,11 @@ public final class R {
         public static final int button_photo=0x7f050003;
         public static final int button_wishlist=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int search=0x7f050007;
+        public static final int title_activity_photo=0x7f050006;
+        public static final int title_activity_results=0x7f050009;
+        public static final int title_activity_website=0x7f05000a;
+        public static final int title_activity_wishlist=0x7f050008;
     }
     public static final class style {
         /** 
--- src/com/cs194/windowshopping/MainActivity.java
+++ src/com/cs194/windowshopping/MainActivity.java
@@ -2,11 +2,12 @@ package com.cs194.windowshopping;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends Activity {
-
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +24,13 @@ public class MainActivity extends Activity {
     
     /*Called when a user clicks the Take Photo button*/
     public void gotoCamera(View view) {
+    	Intent intent = new Intent(this, PhotoActivity.class);
+    	startActivity(intent);
+    }
+    
+    
+    
+    public void onPhotoTaken() {
     	
     }
     
