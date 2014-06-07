196479fe0d53574e7b7daa0c659f12af7d642708
Jordan Marcy <jordan.marcy@gmail.com>
2014-06-07 15:39:22 -0700

    added font file

--- gen/com/cs194/windowshopping/R.java
+++ gen/com/cs194/windowshopping/R.java
@@ -60,13 +60,14 @@ public final class R {
         public static final int retailer_item_view=0x7f030007;
     }
     public static final class menu {
-        public static final int barcode_results=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int photo=0x7f070002;
-        public static final int results=0x7f070003;
-        public static final int retailers=0x7f070004;
-        public static final int website=0x7f070005;
-        public static final int wishlist=0x7f070006;
+        public static final int activity_actions=0x7f070000;
+        public static final int barcode_results=0x7f070001;
+        public static final int main=0x7f070002;
+        public static final int photo=0x7f070003;
+        public static final int results=0x7f070004;
+        public static final int retailers=0x7f070005;
+        public static final int website=0x7f070006;
+        public static final int wishlist=0x7f070007;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
